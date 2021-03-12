5ed7d42d6e0ead2d5f359dd3c970a929c9714238
AlecBruteForce <alecwhite@csus.edu>
2021-03-12 12:39:47 -0800

    removed my code because it was wrong

--- dataDest.py
+++ dataDest.py
@@ -82,8 +82,7 @@ def main_account_screen():
            height="2", width="30",
            command=register).pack()  # calls function register
 
-    Button(text="Exit Application",
-           command=sys.exit(0)).pack()  # Button to exit app
+
 
     main_screen.mainloop()
 
@@ -118,9 +117,7 @@ def login():
     Button(login_screen, text="Login", width=10, height=1,
            command=login_verify).pack()  # calls login_verify to parse user file
 
-    Button(login_screen,
-           text="Exit Application",
-           command=sys.exit(0)).pack()  # Button to exit app
+
 
 # Verifies user login credentials against current list.
 def login_verify():
@@ -942,9 +939,6 @@ def register():
     Button(register_screen, text="Register", width=10, height=1, bg="#42e9f5",
            command=register_Doctor).pack()  # calls register_Doctor
 
-    Button(register_screen,
-           text="Exit Application",
-           command=sys.exit(0)).pack()  # Button to exit app
 
 # This method collects the Doctor's info. Then it saves the user's info into a File.
 def register_Doctor():
